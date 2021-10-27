e32ee39c9770057f0340c3fed8c641e08b9341c5
sumitsingh74081 <Sumit.hangingpanda@gmail.com>
2021-10-27 15:01:25 +0530

    today's code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class Sumit extends React.Component {
   constructor(props)
   {
     super(props);
-    this.state={data:[], get:''}
+    this.state={data:[], get:'',key:""}
     
   };
   handleChange=(e) => {
@@ -18,9 +18,16 @@ class Sumit extends React.Component {
     //const abc=this.state
    if(this.state.get!==''){
       this.setState({data:[...this.state.data,this.state.get]})
-      this.setState({get:''})
       this.setState({text:""})
-   }
+      this.setState({get:''})
+    }
+   
+  }
+  deleteitem(key){
+    const filteritems=this.state.data.filter(data=>
+      data.key!==key);
+      console.log(key);
+      this.setState({data:filteritems})
   }
   
   
@@ -34,18 +41,25 @@ class Sumit extends React.Component {
            <h1>ToDo list</h1>
            <br/>
            <div className="inputdiv">
-           <input type="text" id="type" placeholder="Add a item"  onChange={this.handleChange} value={this.state.text}/>
+           <input type="text" id="type" placeholder="Add a item"  onChange={this.handleChange}
+            value={this.state.data.text}/>
            <button id="click" onClick={this.handleAdd}> + </button><br/>
-           
           {
           data.map((item)=>{
             return <>
+            <div className ="div">
+            <ul>
+            <input type="checkbox" id="check" style={{width:"30px", height:"auto"}}complete/>
             <span>{item}</span>
+            <button id="secondclick" onclick={this.deleteitem}>x</button>
             <br/>
+            </ul>
+            </div>
             </>
 
           })
           }
+          
            </div>
            </div>
     </div>
