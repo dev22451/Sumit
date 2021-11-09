9b9fc425b1936eea5cd9ae3cc862c896989832b3
sumitsingh74081 <Sumit.hangingpanda@gmail.com>
2021-11-10 01:27:24 +0530

    complete todo with gooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
-import Container from "./Container";
-
-function App(){
-  return (
-    <div>
-     <Container/>
+import React,{useState} from "react";
+import Header from "./components/Header";
+import Form from "./components/form";
+import TodoList from "./components/TodoList";
+const App=()=> {
+  const [input,setInput] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [editTodo,setEditTodo] = useState(null);
+  return(
+    <div className = "container">
+      <div className = "app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          setEditTodo={setEditTodo}/>
+        </div>
+      </div>
+      
     </div>
-     
-  );
-};
+  )
+}
 export default App;
